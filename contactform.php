6ac54fc3b7d59851447d058142ff736ffa2148bd
Volkov-da <dmitry.volkov@skoltech.ru>
2020-11-26 04:23:47 +0300

    email sender

--- contactform.php
+++ contactform.php
@@ -1,11 +1,6 @@
 <?php
 if (isset($_POST['submit'])) {
     $email = $_POST['email'];
-
-    $mailTo ="dmtry.volkov@yandex.ru";
-    $headers = "From: ".$email;
-    $txt = "New email on your website!";
-
-    mail($mailTo, $txt, $headers);
-    header("Location: index.php?mailsend");
+    mail($mailTo, 'Subject', 'kek');
 }
+?>
